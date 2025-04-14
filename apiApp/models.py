6f752732f9e9034ffd6a22d7d4fe8e548cca9fcc
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-04-14 21:55:36 +0500

    6th

--- apiApp/models.py
+++ apiApp/models.py
@@ -1,56 +1,62 @@
+from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
+# Create your models here.
+
 
 class CustomUser(AbstractUser):
-     
     email = models.EmailField(unique=True)
     profile_picture_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
-	    return self.email
- 
- 
+        return self.email
+    
+
 class Category(models.Model):
-    name= models.CharField(max_length=255)
-    slug= models.SlugField(unique=True, blank=True)
-    image= models.ImageField( upload_to="category_img",blank=True, null=True)
-    
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to="category_img", blank=True, null=True)
+
     def __str__(self):
         return self.name
-    
+
     def save(self, *args, **kwargs):
+
         if not self.slug:
             self.slug = slugify(self.name)
             unique_slug = self.slug
             counter = 1
             if Product.objects.filter(slug=unique_slug).exists():
-                unique_slug = f"{self.slug}-{counter}"
+                unique_slug = f'{self.slug}-{counter}'
                 counter += 1
+            self.slug = unique_slug
+        
         super().save(*args, **kwargs)
- 
- 
+
+
 class Product(models.Model):
-    name= models.CharField(max_length=255),
-    description= models.TextField(),
-    price= models.DecimalField(max_digits=10, decimal_places=2),
-    slug= models.SlugField(unique=True, blank=True)
-    image= models.ImageField( upload_to="product_img",blank=True, null=True)
-    featured= models.BooleanField(default=False)
-    category= models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="products", null=True, blank=True)
-    
-    
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to="product_img", blank=True, null=True)
+    featured = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="products",  blank=True, null=True)
+
     def __str__(self):
         return self.name
+    
     def save(self, *args, **kwargs):
+
         if not self.slug:
             self.slug = slugify(self.name)
             unique_slug = self.slug
             counter = 1
             if Product.objects.filter(slug=unique_slug).exists():
-                unique_slug = f"{self.slug}-{counter}"
+                unique_slug = f'{self.slug}-{counter}'
                 counter += 1
+            self.slug = unique_slug
+        
         super().save(*args, **kwargs)
- 
-    
